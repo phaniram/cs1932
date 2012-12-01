0c499d8a5f13a78bc09ba7fc9f25d34294e335a8
maturu srinivasphaniram <maturusrinivasphaniram@gmail.com>
2012-12-02 02:55:00 +0530

    Initial Version

---
+++ src/csHulk.java
@@ -0,0 +1,9 @@
+import org.apache.log4j.Logger;
+
+public class csHulk {
+	private final static Logger LOGGER = Logger.getLogger(csHulk.class.getName());
+    public static void main(String[] args) {
+    	LOGGER.info("info level log test");
+    	System.out.println("Sysout log");
+    }
+}
