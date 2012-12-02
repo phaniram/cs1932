3f550032e53c99a8f504ac423e83686b4424a631
maturu srinivasphaniram <maturusrinivasphaniram@gmail.com>
2012-12-02 13:25:32 +0530

    modified:   build.xml 	modified:   src/csHulk.java

--- src/csHulk.java
+++ src/csHulk.java
@@ -2,12 +2,13 @@ import org.apache.log4j.Logger;
 
 public class csHulk {
 	private final static Logger LOGGER = Logger.getLogger(csHulk.class.getName());
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
     	LOGGER.info("info level log test");
     	LOGGER.info("In loop");
     	for(int i=1;i<11;i++)
     	{
-    		LOGGER.debug("loop run:"+i);
+    		Thread.sleep(2000);
+    		LOGGER.debug(" run:"+i);
     	}
     	LOGGER.info("Out of the loop");
     	System.out.println("Sysout log");
