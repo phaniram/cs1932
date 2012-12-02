7faa2da016ed0e67499f841637063c3a16082149
maturu srinivasphaniram <maturusrinivasphaniram@gmail.com>
2012-12-02 13:08:48 +0530

    midfin

--- src/csHulk.java
+++ src/csHulk.java
@@ -4,6 +4,12 @@ public class csHulk {
 	private final static Logger LOGGER = Logger.getLogger(csHulk.class.getName());
     public static void main(String[] args) {
     	LOGGER.info("info level log test");
+    	LOGGER.info("In loop");
+    	for(int i=1;i<11;i++)
+    	{
+    		LOGGER.debug("loop run:"+i);
+    	}
+    	LOGGER.info("Out of the loop");
     	System.out.println("Sysout log");
     }
 }
